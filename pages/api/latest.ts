8c26d1cd64c4ed9e7618a6ff41161f3263848d55
johnwalley <john@walley.org.uk>
2022-07-10 10:52:27 +0100

    feat: new paths

--- pages/api/latest.ts
+++ pages/api/latest.ts
@@ -1,7 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import results from "../../../../results.json";
+import events from "./events.json";
+import { transformData, joinEvents } from "bumps-results-tools";
 import Joi, { ValidationError } from "joi";
+
+const GENDER = {
+  MEN: "Men",
+  WOMEN: "Women",
+};
+
+const SET = {
+  EIGHTS: "Summer Eights",
+  TORPIDS: "Torpids",
+  LENTS: "Lent Bumps",
+  MAYS: "May Bumps",
+  TOWN: "Town Bumps",
+};
+
+const set = {
+  eights: SET.EIGHTS,
+  torpids: SET.TORPIDS,
+  lents: SET.LENTS,
+  mays: SET.MAYS,
+  town: SET.TOWN,
+};
 
 const schema = Joi.object({
   event: Joi.any()
@@ -16,7 +38,7 @@ type Data = {
   crews: any[];
   divisions: any[];
   startYear: number;
-};
+}[];
 
 export default function handler(
   req: NextApiRequest,
@@ -25,40 +47,22 @@ export default function handler(
   try {
     Joi.assert(req.query, schema);
 
-    const {
-      event = "mays",
-      gender = "women",
-      start = Number.NEGATIVE_INFINITY,
-      end = Number.POSITIVE_INFINITY,
-    } = req.query;
+    const { event = "mays", gender = "women" } = req.query;
 
-    const data = (results as any[])
+    const data = (events as any[])
       .filter(
         (d) => d.gender.toLowerCase() === (gender as string).toLowerCase()
       )
-      .filter(
-        (d) => d.small.toLowerCase() === (event as string).toLowerCase()
-      )[0];
+      .filter((d) => d.small.toLowerCase() === (event as string).toLowerCase());
 
-    const dataRange = {
-      set: data.set,
-      gender: data.gender,
-      crews: data.crews
-        .filter((crew: any) => crew.values[crew.values.length - 1].pos !== -1)
-        .map((crew: any) => ({
-          name: crew.name,
-          values: crew.values
-            .slice(-5)
-            .map((v: any, i: number) => ({ day: i, pos: v.pos })),
-          valuesSplit: crew.valuesSplit.slice(-1),
-        })),
-      divisions: data.divisions.slice(-1),
-    };
+    const latestEvent = transformData(data[data.length - 1]);
+    const joinedEvents = joinEvents([latestEvent], event, gender);
 
-    res.status(200).json({
-      ...dataRange,
-      startYear: data.divisions[data.divisions.length - 1].year,
-    });
+    joinedEvents.small = event;
+    joinedEvents.gender = gender;
+    joinedEvents.set = set[event as keyof typeof set];
+
+    res.status(200).json(joinedEvents);
   } catch (error) {
     let message = "There was an error";
 
