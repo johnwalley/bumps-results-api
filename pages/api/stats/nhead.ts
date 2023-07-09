cd62e489f636acf3cf63dfcc6bb65b53a00b4fbc
johnwalley <john@walley.org.uk>
2023-07-09 18:01:25 +0100

    Initial stats

---
+++ pages/api/stats/nhead.ts
@@ -0,0 +1,64 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import stats from "../stats.json";
+import { transformData, joinEvents } from "bumps-results-tools";
+import Joi, { ValidationError } from "joi";
+
+const GENDER = {
+  MEN: "Men",
+  WOMEN: "Women",
+};
+
+const SET = {
+  EIGHTS: "Summer Eights",
+  TORPIDS: "Torpids",
+  LENTS: "Lent Bumps",
+  MAYS: "May Bumps",
+  TOWN: "Town Bumps",
+};
+
+const set = {
+  eights: SET.EIGHTS,
+  torpids: SET.TORPIDS,
+  lents: SET.LENTS,
+  mays: SET.MAYS,
+  town: SET.TOWN,
+};
+
+const schema = Joi.object({
+  event: Joi.any()
+    .valid("eights", "lents", "mays", "torpids", "town")
+    .required(),
+  gender: Joi.any().valid("men", "women").required(),
+});
+
+type Data = {
+  set: string;
+  gender: string;
+  crews: any[];
+  divisions: any[];
+  startYear: number;
+}[];
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data | { message: string }>
+) {
+  try {
+    Joi.assert(req.query, schema);
+
+    const { event = "mays", gender = "women" } = req.query;
+
+    const nhead = stats["nhead"] as any;
+
+    res.status(200).json(nhead[event as string][gender as string]);
+  } catch (error) {
+    let message = "There was an error";
+
+    if (error instanceof ValidationError) {
+      message = error.details.map((detail) => detail.message).join(", ");
+    }
+
+    res.status(400).json({ message });
+  }
+}
